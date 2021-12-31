5ef6927457ccb8315443608af69d9c29ac58fcad
jkaravadiya753 <jkaravadiya753@rku.ac.in>
2021-12-31 09:35:49 +0530

    Second File

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var server = http.createServer(function(req,res){
     res.writeHead(200,{"context-type":"text/html"});
-    res.write("hello")
+    res.write("<h1>hello</h1>")
     res.end();
 }).listen(2000);
 console.log("server started on 2000");
